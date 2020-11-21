6fc9922301616eb2caad62e862fada240bfe9bdf
hakanyacinkaya <hakanyalcinkaya47@gmail.com>
2020-11-21 01:11:58 +0100

    Das Wirken werden geschrieben.

--- berkel/crud/models.py
+++ berkel/crud/models.py
@@ -4,8 +4,10 @@ from django.db import models
 class Customer(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-    city = models.CharField(max_length=255)
     age = models.IntegerField()
+
+    def __str__(self):
+        return self.first_name + " - " + self.last_name + " - " + str(self.age)
 
 
 class Item(models.Model):
--- berkel/crud/script.py
+++ berkel/crud/script.py
@@ -3,6 +3,11 @@ from crud.models import Customer
 
 print("""
     1. Customer erstellen
+    2. Customer Liste
+    3. Customer Detail
+    4. Customer Aktualisieren
+    5. Customer Löschen
+    6. Customer Suchen
 """)
 
 user_input = int(input('Was möchtest du machen?'))
@@ -12,3 +17,30 @@ if user_input == 1:
     last_name = input('Nachname: ')
     age = int(input('Alter: '))
     Customer(first_name=first_name, last_name=last_name, age=age).save()
+
+elif user_input == 2:
+    all_entries = Customer.objects.all()
+    for entity in all_entries:
+        print(entity)
+
+elif user_input == 3:
+    id = int(input("Bitte, Sie schreiben Id: "))
+    entity = Customer.objects.get(pk=id)
+    print(entity)
+
+# elif user_input==4:
+
+elif user_input == 5:
+    id = int(input("Bitte, Sie schreiben Id: "))
+    entity = Customer.objects.get(pk=id)
+    entity.delete()
+
+
+elif user_input == 6:
+    input_text = input("Bitte, Sie schreiben : ")
+    all_entries = Customer.objects.filter(first_name=input_text)
+    #  = Customer.objects.filter(last_name=input_text)
+    for entity in all_entries:
+        print(entity)
+
+#
