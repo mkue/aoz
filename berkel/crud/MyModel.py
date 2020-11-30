62c6287570e40b7ab59bce7e2d3e1205d9471b4b
Michael Kündig <michael.kuendig@gmail.com>
2020-11-30 17:36:30 +0100

    Add covid_cases

--- berkel/crud/MyModel.py
+++
@@ -1,5 +0,0 @@
-
-class Product:
-
-    def __init__(self, id):
-        self.id = id
--- berkel/crud/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from crud.models import Customer
-
-admin.site.register(Customer)
